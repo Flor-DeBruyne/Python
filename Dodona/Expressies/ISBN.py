65960d665c62d9f7cd257961f607a46426c1242f
Flor De Bruyne <flor.debruyne@student.hogent.be>
2021-10-06 15:51:36 +0200

    2 excercise

--- Dodona/Expressies/ISBN.py
+++ Dodona/Expressies/ISBN.py
@@ -1,8 +1,8 @@
-import array as arr #You need to import array module
 a = [0 for x in range(9)]
-
+laatste = 0
 for x in range(9):
   a[x] = int(input())
-print(a)
-laatste = (sum(a))  %11
+  laatste += (a[x]*x)
+  
 print(laatste)
+print(laatste %11)
---
+++ Dodona/Expressies/ThePuddingGuy.py
@@ -0,0 +1,6 @@
+stuks = int(input())
+kostprijs = float(input())
+barcodes = int(input())
+mijlen = int(input())
+
+print("Phillips spendeerde {prijs} voor {totaal} frequent flyer mijlen.",format(stuks*kostprijs,barcodes * mijlen))
