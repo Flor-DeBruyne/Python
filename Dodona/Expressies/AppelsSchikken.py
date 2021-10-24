11ca4f111c3f45e5db48765e3b44da2c0e8869aa
Flor De Bruyne <flor.debruyne@student.hogent.be>
2021-10-24 17:25:52 +0200

    need to commit first cannot pull with rebase

--- Dodona/Expressies/AppelsSchikken.py
+++ Dodona/Expressies/AppelsSchikken.py
@@ -1,5 +1,5 @@
 appels = int(input())
 kisten = int(appels //20)
-pallenten = int(kisten //35)
+palleten = int(kisten //35)
 
-print("{}\n{}\n{}".format(pallenten,kisten,appels))
+print(f"{palleten}\n{kisten}\n{appels}")
