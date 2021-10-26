8e1fbaab3fba60b54f6e90d9b33fc7e973124878
Flor De Bruyne <flor.debruyne@student.hogent.be>
2021-10-26 04:34:46 +0200

    little coding

---
+++ Dodona/Functions/hearingLoss.py
@@ -0,0 +1,37 @@
+def maximum_exposure(decibel=40):
+  """calculating the maximum expore in seconds for the amount of decibel"""
+  eightHours = 28800.0
+  
+
+
+
+
+
+
+
+
+  
+# match decibel: #match is only available in Python 3.10 and up (error now on dodona)
+#   case n if n in {80,81,82}:
+#     return eightHours
+#   case n if n in {83,84,85}:
+#     return eightHours/2
+#   case n if n in {86,87,88}:
+#     return eightHours/4
+#   case n if n in {89,90,91}:
+#     return eightHours/6
+#   case n if n in {92,93,94}:
+#     return eightHours/8
+#   case n if n in {95,96,97}:
+#     return eightHours/10
+#   case n if n in {98,99,100}:
+#     return eightHours/12
+#   case n if n > 100:
+#     return eightHours/14
+#   case _:
+#     return -1
+
+
+
+  
+  
