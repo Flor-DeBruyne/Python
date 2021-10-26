abad2ca0a5fbac8e65b196d7065176d40e3ad931
Flor De Bruyne <flor.debruyne@student.hogent.be>
2021-10-26 18:38:55 +0200

    worked in kantienberg

---
+++ Dodona/Functions/BigMacIndex.py
@@ -0,0 +1,39 @@
+def appreciation(priceBigMac: float, exchangeRate = 0.70):
+    """Calculating the appreciation of the big mac"""
+    usPrice = 4.07
+    calcExchange = priceBigMac/usPrice
+    v = (calcExchange/exchangeRate) * 100
+    if v <= -25:
+      return "strongly underrated"
+    elif v > -25 and v <= -5:
+      return "underrated"
+    elif v > -5 and v <= 5:
+      return "about equal"
+    elif v > 5 and v <= 25:
+      return "overrated"
+    else:
+      return "strongly overrated"
+    # match v:
+    #   case n if n <= -25:
+    #     return "strongly underrated"
+    #   case n if n > -25 and n <= -5:
+    #     return "underrated"
+    #   case n if n  > -5 and n <= 5:
+    #     return "about equal"
+    #   case n if n > 5 and n <= 25:
+    #     return "overrated"
+    #   case n if n > 25:
+    #     return "strongly overrated"
+
+def exchange_rate_analysis(priceBig: str, exchangeRate = 0.70):
+    """printing the appreciation rate of the big"""
+    price = priceBig.split(' ')
+    currency = price[1:]
+    listToString = ''.join(map(str,currency))
+    print(f"the {listToString} is {appreciation(float(price[0]))} with regard to the dollar.")
+
+
+
+
+
+
--- Dodona/Functions/hearingLoss.py
+++ Dodona/Functions/hearingLoss.py
@@ -1,7 +1,36 @@
 def maximum_exposure(decibel=40):
   """calculating the maximum expore in seconds for the amount of decibel"""
   eightHours = 28800.0
-  
+  if decibel <80:
+    return -1.0
+  elif decibel in {80,81,82}:
+     return eightHours
+  elif decibel in {83,84,85}:
+    return eightHours/2
+  elif decibel in {86,87,88}:
+    return eightHours/4
+  elif decibel in {89,90,91}:
+    return eightHours/8
+  elif decibel in {92,93,94}:
+    return eightHours/16
+  elif decibel in {95,96,97}:
+    return eightHours/32
+  elif decibel in {98,99,100}:
+    return eightHours/64
+  elif decibel in {101,102,103}:
+    return eightHours/128
+  elif decibel in {104,105,106}:
+    return eightHours/256
+  elif decibel in {107,108,109}:
+    return eightHours/512
+  elif decibel in {110,111,112}:
+    return eightHours/1024
+  elif decibel in {113,114,115}:
+    return 14.0625
+  elif decibel in {116,117,118}:
+    return 7.03125
+  else:
+    return 3.515625
 
 
 
